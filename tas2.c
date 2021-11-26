79535d71ccd6994ddeb9c62c4a23e921ff5e3070
RabeyaShammi <rabeya15-12145@diu.edu.bd>
2021-11-26 20:41:36 +0600

    project version one

---
+++ tas2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    char str1[1000], str2[1000];
+    int i,j=0,len;
+
+    printf("Input string: ");
+    gets(str1);
+
+    len = strlen(str1);
+
+    for(i=0; i<len; i++){
+        if(str1[i] != ' '){
+            str2[j++] = str1[i];
+        }
+
+    }
+    str2[j] = '\0';
+    printf("\n%s", str2);
+    return 0;
+}
